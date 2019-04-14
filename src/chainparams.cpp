b8ac252e2c342c3f2168a4f84c42b57449a73aac
CircuitBreaker88 <sarris88p@gmail.com>
2019-04-14 06:00:12 -0400

    seed nodes addition

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -183,6 +183,9 @@ public:
 
         vSeeds.push_back(CDNSSeedData("140.82.36.171", "140.82.36.171"));
         vSeeds.push_back(CDNSSeedData("45.76.21.144", "45.76.21.144"));
+        vSeeds.push_back(CDNSSeedData("45.77.22.11", "45.77.22.11"));
+        vSeeds.push_back(CDNSSeedData("140.82.49.169", "140.82.49.169"));
+        vSeeds.push_back(CDNSSeedData("149.28.206.179", "149.28.206.179"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 60); // RupeeEvolution's wallet address starts with a R
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 15); // 7
